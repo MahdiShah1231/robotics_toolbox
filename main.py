b9ca01c49d1a5361852015a17ce05200627f752a
MahdiShah1231 <shah.mahdi@hotmail.com>
2022-11-30 17:26:36 +0000

    Reverted moving creation functions to helper_functions.py due to circular import issues.

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import numpy as np
+from Robot import Robot
 from InverseKinematics import Fabrik
-from helper_functions.helper_functions import create_robot, forward_kinematics, inverse_kinematics
+
+def create_robot(link_lengths,
+                 ik_alg=None,
+                 joint_configuration=None,
+                 robot_base_radius=None,
+                 linear_base=False,
+                 environment=None):
+
+    robot = Robot(link_lengths=link_lengths,
+                  ik_alg=ik_alg,
+                  joint_configuration=joint_configuration,
+                  robot_base_radius=robot_base_radius,
+                  linear_base=linear_base,
+                  environment=environment)
+    return robot
+
+
+def inverse_kinematics(robot,
+                       target_position,
+                       target_orientation=None,
+                       mirror=True,
+                       debug=False,
+                       plot=True):
+
+    robot.inverse_kinematics(target_position=target_position,
+                             target_orientation=target_orientation,
+                             mirror=mirror,
+                             debug=debug,
+                             plot=plot)
+
+
+def forward_kinematics(robot, target_configuration, debug=False, plot=True):
+    robot.forward_kinematics(target_configuration=target_configuration, debug=debug, plot=plot)
+
 
 if __name__ == '__main__':
     r = create_robot(link_lengths=[0.4, 0.3, 0.2],
@@ -9,6 +43,5 @@ if __name__ == '__main__':
                      robot_base_radius=0.1,
                      linear_base=True,
                      environment=None)
-    # inverse_kinematics(robot=r, target_position=[1, 0.6], target_orientation=np.pi / 2, mirror=True, debug=True)
-    # forward_kinematics(robot=r, target_configuration=[0, np.pi/2, -np.pi/2])
-    # forward_kinematics(robot=r, target_configuration=[0, 0, 0])
+    # inverse_kinematics(robot=r, target_position=[1, 0.6], target_orientation=np.pi / 2, mirror=True, debug=False)
+    forward_kinematics(robot=r, target_configuration=[0, np.pi/2, -np.pi/2], debug=False)
